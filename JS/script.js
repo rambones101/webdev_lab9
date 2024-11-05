4275ad31bdbb68ab9a042fc005fe4351a371303c
rambones101 <erickedram@gmail.com>
2024-11-05 15:42:31 -0600

    fixed button

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,13 @@
-function init() {
-  //add your javascrip between these two lines of code
-
-  var button = document.getElementById("entrybutton");
-
-  function showMeText() {
-    var textbox = document.getElementById("entryinput");
-    document.getElementById("textoutput").innerHTML = textbox.value;
-    alert("Erick Ramirez: " + textbox.value);
-  }
-
-  button.addEventListener("click", showMeText);
-}
-
-window.addEventListener("load", init);
+function init(){
+    var button = document.getElementById('entrybutton');
+  
+    function showMeText(){
+      var textbox = document.getElementById('entryinput');
+      document.getElementById('textoutput').innerHTML = textbox.value;
+      alert("Erick Ramirez: " + textbox.value);
+    }
+  
+    button.addEventListener('click', showMeText);
+    }
+    
+  window.addEventListener('load', init);
